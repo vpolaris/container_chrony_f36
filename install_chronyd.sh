857e07b82d16c5e21d840ffdcc8429c8299c32a0
Olivier Sylva <szfd9g@live.fr>
2022-01-03 03:04:15 +0100

    typo error

--- install_chronyd.sh
+++ install_chronyd.sh
@@ -19,12 +19,12 @@ CHRONYVERSION=$(cat ${sysroot}/chrony.version)
 podman rmi f33:builder
 podman build --squash-all -t chrony:${CHRONYVERSION}-${proc} -f ./Dockerfile.chrony
 if [[ $(podman volume exists run_chrony;echo $?) -eq 1 ]]; then  
-podman volume create --opt device=tmpfs --opt type=tmpfs --opt o=nodev,noexec,uid=500,gid=500,mode=1750,size=4K run_chrony
-;fi
+    podman volume create --opt device=tmpfs --opt type=tmpfs --opt o=nodev,noexec,uid=500,gid=500,mode=1750,size=4K run_chrony;
+fi
 
 if [[ $(podman volume exists var_chrony;echo $?) -eq 1 ]]; then  
-podman volume create --opt device=tmpfs --opt type=tmpfs --opt o=nodev,noexec,uid=500,gid=500,mode=1750,size=1M var_chrony
-;fi
+    podman volume create --opt device=tmpfs --opt type=tmpfs --opt o=nodev,noexec,uid=500,gid=500,mode=1750,size=1M var_chrony;
+fi
 
 podman run -ti --rm  --read-only  \
     --name chrony \
